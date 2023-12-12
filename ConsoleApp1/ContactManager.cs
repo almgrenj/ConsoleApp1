b40d384ac405d3b91c647f426e2e85ebba649ed9
Jacob Almgren <almgrenj@proton.me>
2023-12-12 11:55:26 +0100

    updated

--- ConsoleApp1/ContactManager.cs
+++ ConsoleApp1/ContactManager.cs
@@ -39,7 +39,7 @@ namespace ConsoleApp1.Services
         }
 
         /// <summary>
-        /// Sparar nuvarande kontakter till en fil.
+        /// Sparar nuvarande kontaskter till en fil.
         /// </summary>
         public void SaveContacts()
         {
